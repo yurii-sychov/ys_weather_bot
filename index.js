3f4efcff1dc4a028da8bfa58402f879750f0f4ae
Yurii Sychov <yurii@sychov.pp.ua>
2025-03-17 09:34:11 +0200

    2025-03-17_09-34-15

--- index.js
+++ index.js
@@ -13,15 +13,28 @@ bot.api.setMyCommands([
 
 bot.command("start", async (ctx) => await ctx.reply("Привет, i bot!"));
 
+// bot.on(":location", async (ctx) => {
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.msg.location.latitude}&lon=${ctx.msg.location.longitude}&appid=87f4403542476da41e8e2480e6b95de4&units=metric`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data);
+//     ctx.reply(`${data.name}: ${data.main.temp} C`);
+// });
+
 bot.on(":location", async (ctx) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.msg.location.latitude}&lon=${ctx.msg.location.longitude}&appid=87f4403542476da41e8e2480e6b95de4&units=metric`;
+    console.log(url);
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
-    ctx.reply(`${data.name}: ${data.main.temp} C`);
+    ctx.reply(`<strong>${data.name}:</strong> ${data.main.temp} C <a href="https://arctic.pp.ua/">Repair</a>`,
+        {
+            parse_mode: "HTML"
+        });
 });
 
 bot.on("message", async (ctx) => {
+    console.log(ctx.msg);
     ctx.reply('Send Location');
 });
 
